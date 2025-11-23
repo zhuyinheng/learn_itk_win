c2ac9d6573901f8332173a9874102c1f1688a6e1
Your Name <zhuyinheng666@gmail.com>
2025-11-23 02:16:18 -0600

    toolchain MSVC, fetch content integrate itk

--- src/learn_itk_win.h
+++ src/learn_itk_win.h
@@ -1,15 +1,10 @@
-﻿// learn_itk_win.h: 标准系统包含文件的包含文件
-// 或项目特定的包含文件。
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
 
-// ITK 相关头文件 - 项目始终使用 ITK
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 #include "itkRescaleIntensityImageFilter.h"
 #include "itkImageRegionIterator.h"
 
-// TODO: 在此处引用程序需要的其他标头。
